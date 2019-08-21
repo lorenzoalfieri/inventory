b05b5b97d4e6c9fbcba45298af38a1758939f085
lorenzoalfieri <lorenzo.alfieri@epitech.eu>
2019-08-21 12:25:29 +0200

    remove useless file and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
 import Inventory from "./components/Inventory";
 
 const App = () => (
   <main>
-    <Switch>
-      <Route exact path="/" component={Inventory} />
-    </Switch>
+    <Inventory />
   </main>
 );
 
